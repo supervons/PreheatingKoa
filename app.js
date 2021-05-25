1e6c3934abdb96444b6557b727591d302b191e26
supervons <supervons@sina.com>
2021-05-25 22:58:45 +0800

    add post request

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const koa = require('koa');
 const app = new koa();
+
+// add a post middleware.use curl -d "params1=value1" http://localhost:3000/ test
 app.use(async (ctx, next) => {
-  await next();
-  ctx.response.type = 'text/html';
-  ctx.response.body = '<h1>Hello World</h1>';
+  let postdata = '';
+  ctx.req.on('data', data => {
+    postdata += data;
+  });
+  ctx.req.on('end', () => {
+    console.log(postdata);
+  });
 });
+
+// add a middleware.
+// app.use(async (ctx, next) => {
+//   await next();
+//   ctx.response.type = 'text/html';
+//   ctx.response.body = {
+//     url: ctx.request.url,
+//     query: ctx.request.query,
+//     querystring: ctx.request.querystring
+//   };
+// });
+
+// add a listen.
 app.listen(3000, () => {
   console.log('server is running at http://localhost:3000');
 });
