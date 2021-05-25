108423f82b38cc8cbee553d32eb945afa6a1b098
supervons <supervons@sina.com>
2021-05-26 00:01:23 +0800

    add response type

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 const koa = require('koa');
 const app = new koa();
 
-// add a post middleware.use curl -d "params1=value1" http://localhost:3000/ test
-app.use(async (ctx, next) => {
-  let postdata = '';
-  ctx.req.on('data', data => {
-    postdata += data;
-  });
-  ctx.req.on('end', () => {
-    console.log(postdata);
-  });
-});
-
 // add a middleware.
-// app.use(async (ctx, next) => {
-//   await next();
-//   ctx.response.type = 'text/html';
-//   ctx.response.body = {
-//     url: ctx.request.url,
-//     query: ctx.request.query,
-//     querystring: ctx.request.querystring
-//   };
-// });
+app.use(async (ctx, next) => {
+  ctx.response.status = 200;
+  // choose method
+  if (ctx.request.method === 'POST') {
+    let postdata = '';
+    ctx.req.on('data', data => {
+      postdata += data;
+    });
+    ctx.req.on('end', () => {
+      console.log(postdata);
+    });
+  } else if (ctx.request.method === 'GET') {
+    if (ctx.request.path !== '/') {
+      ctx.response.type = 'text/html';
+      ctx.response.body = {
+        url: ctx.request.url,
+        query: ctx.request.query,
+        querystring: ctx.request.querystring
+      };
+    } else {
+      // add response type : text、html、json
+      ctx.response.type = 'text';
+      ctx.response.body = { name: 'Hello World1' };
+    }
+  }
+});
 
 // add a listen.
 app.listen(3000, () => {
