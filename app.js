a35c1eea4d9f9f76f5be1262fcc5af71fceb0693
supervons <supervons@sina.com>
2021-05-25 22:26:06 +0800

    add first api and use prettier for code.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('123')
+const koa = require('koa');
+const app = new koa();
+app.use(async (ctx, next) => {
+  await next();
+  ctx.response.type = 'text/html';
+  ctx.response.body = '<h1>Hello World</h1>';
+});
+app.listen(3000, () => {
+  console.log('server is running at http://localhost:3000');
+});
