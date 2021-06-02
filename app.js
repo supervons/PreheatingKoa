8c7e2df170d94a5444df326fede777974bf64391
supervons <supervons@sina.com>
2021-06-02 19:47:37 +0800

    add koa-route and specification interface

--- app.js
+++ app.js
@@ -1,54 +1,39 @@
 const koa = require('koa');
+const route = require('koa-route');
 const app = new koa();
 
-// add a middleware.
-app.use(async (ctx, next) => {
-  console.log(app);
-  ctx.response.status = 200;
-  // Execute the next middleware
-  await next();
-  // choose method
-  if (ctx.request.method === 'POST') {
-    let postdata = '';
-    ctx.req.on('data', data => {
-      postdata += data;
-    });
-    ctx.req.on('end', () => {
-      console.log(postdata);
-    });
-  } else if (ctx.request.method === 'GET') {
-    if (ctx.request.path === '/getHtml') {
-      // read template file.
-      const fs = require('fs');
-      ctx.response.type = 'html';
-      ctx.response.body = fs.createReadStream('./template/test.html');
-    } else if (ctx.request.path !== '/') {
-      ctx.response.type = 'text/html';
-      ctx.response.body = {
-        url: ctx.request.url,
-        query: ctx.request.query,
-        querystring: ctx.request.querystring
-      };
-    } else {
-      // root path add response type : text、html、json
-      ctx.response.type = 'text';
-      ctx.response.body = { name: 'Hello World1' };
-    }
-  }
-});
+const getHtml = ctx => {
+  const fs = require('fs');
+  ctx.response.type = 'html';
+  ctx.response.body = fs.createReadStream('./template/test.html');
+};
 
-// middleware2
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
-});
+// get request
+const getText = ctx => {
+  ctx.response.type = 'text/html';
+  ctx.response.body = {
+    url: ctx.request.url,
+    query: ctx.request.query,
+    querystring: ctx.request.querystring
+  };
+};
 
-// middleware3
-app.use(async (ctx, next) => {
-  console.log(`i'm last middleware`);
-});
+// post request test
+const postTest = async ctx => {
+  ctx.response.type = 'application/json';
+  let postdata = '';
+  await ctx.req.on('data', data => {
+    postdata += data;
+  });
+  ctx.req.on('end', () => {
+    console.log(postdata);
+  });
+  ctx.response.body = postdata;
+};
+
+app.use(route.get('/getHtml', getHtml));
+app.use(route.get('/getText', getText));
+app.use(route.post('/postTest', postTest));
 
 // add a listen.
 app.listen(3000, () => {
