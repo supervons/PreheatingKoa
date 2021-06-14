71f85e4c88e6d090d762ae6748ad9d94f5449283
supervons <supervons@sina.com>
2021-06-14 14:49:03 +0800

    use mysql.escape Preventing SQL Injection.

--- data_base/index.js
+++ data_base/index.js
@@ -19,15 +19,21 @@ class Mysql {
    * get news info
    * @returns news list
    */
-  queryNews() {
+  queryNews(params) {
+    console.log(mysql.escape(params.title));
     return new Promise((resolve, reject) => {
-      pool.query('SELECT * from news_info', function (error, results, fields) {
-        if (error) {
-          throw error;
+      pool.query(
+        `SELECT * from news_info where news_title =${mysql.escape(
+          params.title
+        )} and news_time ='北京'`,
+        function (error, results, fields) {
+          if (error) {
+            throw error;
+          }
+          resolve(results);
+          // console.log('The solution is: ', results[0].solution);
         }
-        resolve(results);
-        // console.log('The solution is: ', results[0].solution);
-      });
+      );
     });
   }
 
@@ -48,4 +54,7 @@ class Mysql {
   }
 }
 
-module.exports = new Mysql();
+module.exports = {
+  mysql: new Mysql(),
+  escape: mysql.escape
+};
--- routes/demo.js
+++ routes/demo.js
@@ -1,5 +1,5 @@
 const router = require('koa-router')();
-const mysql = require('../data_base/index');
+const { mysql } = require('../data_base/index');
 const redis = require('../config/redis/index');
 const redisTool = new redis();
 
@@ -7,11 +7,12 @@ const redisTool = new redis();
 const mysqlTest = async ctx => {
   // query redis data. if redis exist, don't query database.
   const redisNewsData = await redisTool.get('news');
+  console.log(ctx.request.body.title);
   let newsData = [];
   if (redisNewsData) {
     newsData = JSON.parse(redisNewsData);
   } else {
-    let data = await mysql.queryNews();
+    let data = await mysql.queryNews({ title: ctx.request.body.title });
     newsData = data;
     // update redis data.
     await redisTool.set('news', JSON.stringify(data));
